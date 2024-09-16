ec878630a9d46ec7695ce35184f358c36cae7314
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-09-16 14:52:52 +0300

    added react and new component

---
+++ src/lib/hello/hello.tsx
@@ -0,0 +1,26 @@
+
+
+
+
+
+  import styles from './hello.module.css';
+  
+
+
+
+  
+  export function Hello() {
+    return (
+      <div className={styles['container']}>
+        
+        <h1>Welcome to Hello!</h1>
+        
+      </div>
+    );
+  }
+  
+
+  export default Hello;
+
+
+
